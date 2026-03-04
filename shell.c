06c2993bd2b64db208aaf21c1ac269345d1ffccb
Daniel S. <danielbarbosa27@icloud.com>
2026-03-04 18:58:50 -0300

    basic return

--- shell.c
+++ shell.c
@@ -3,22 +3,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int main(){
-    pid_t pid_code;
-    int n=20;
-
-    pid_code = fork();
-
-    if(pid_code < 0){
-        printf("Ish rapaz deu erro");
-        exit();
-    }else if(pid_code==0){
-        n=10;
-        printf("n-filho =%d\n",n);
-    }else if(pid_code>0){
-        printf("n-pai =%d\n",n);
-    }
     return 0;
 }
