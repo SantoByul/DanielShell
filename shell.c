f3bcaf313799219c78bedc4486de89b86771abdb
Daniel S. <danielbarbosa27@icloud.com>
2026-03-04 21:34:27 -0300

    simple hello world

--- shell.c
+++ shell.c
@@ -1,11 +1,31 @@
 //gcc shell.c -o shell
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(){
+    char comando[100];
+
+    while(1){
+        printf("myshell> ");
+        fflush(stdout);
+
+        if(fgets(comando, sizeof(comando), stdin) == NULL)
+            break;
+
+        comando[strcspn(comando, "\n")] = 0;
+
+        if(strcmp(comando, "exit") == 0)
+            break;
+
+        if(strcmp(comando, "hello") == 0){
+            printf("hello world\n");
+        } else {
+            printf("comando nao reconhecido\n");
+        }
+    }
+
     return 0;
 }
