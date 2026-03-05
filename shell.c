96c85725dd3be7d12eecb39edb27cd8c78c5ec76
Daniel S. <danielbarbosa27@icloud.com>
2026-03-05 08:24:01 -0300

    reset( de novo)

--- shell.c
+++ shell.c
@@ -1,4 +1,5 @@
 //gcc shell.c -o shell
+//Lembrar do fork() exec() wait() dup2() pipe()
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,47 +7,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX 1024
-#define MAX_ARGS 100
-
 int main(){
-    char linha[MAX];
-    char *args[MAX_ARGS];
-
-    while(1){
-        printf("myshell> ");
-        fflush(stdout);
-
-        if(fgets(linha, MAX, stdin) == NULL)
-            break;
-
-        linha[strcspn(linha, "\n")] = 0;
-
-        if(strcmp(linha, "exit") == 0)
-            break;
-
-        int i = 0;
-        char *token = strtok(linha, " ");
-        while(token != NULL && i < MAX_ARGS - 1){
-            args[i++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[i] = NULL;
-
-        pid_t pid = fork();
-
-        if(pid == 0){
-            execvp(args[0], args);
-            perror("erro");
-            exit(1);
-        } 
-        else if(pid > 0){
-            wait(NULL);
-        } 
-        else{
-            perror("fork falhou");
-        }
-    }
-
     return 0;
 }
