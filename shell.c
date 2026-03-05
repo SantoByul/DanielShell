dc78143ed76761ac854cadd78305db41bcb3b3ac
Daniel S. <danielbarbosa27@icloud.com>
2026-03-04 21:45:20 -0300

    Boo
    Aprendi a usar a droga do wsl

--- shell.c
+++ shell.c
@@ -4,26 +4,47 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX 1024
+#define MAX_ARGS 100
 
 int main(){
-    char comando[100];
+    char linha[MAX];
+    char *args[MAX_ARGS];
 
     while(1){
         printf("myshell> ");
         fflush(stdout);
 
-        if(fgets(comando, sizeof(comando), stdin) == NULL)
+        if(fgets(linha, MAX, stdin) == NULL)
             break;
 
-        comando[strcspn(comando, "\n")] = 0;
+        linha[strcspn(linha, "\n")] = 0;
 
-        if(strcmp(comando, "exit") == 0)
+        if(strcmp(linha, "exit") == 0)
             break;
 
-        if(strcmp(comando, "hello") == 0){
-            printf("hello world\n");
-        } else {
-            printf("comando nao reconhecido\n");
+        int i = 0;
+        char *token = strtok(linha, " ");
+        while(token != NULL && i < MAX_ARGS - 1){
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        pid_t pid = fork();
+
+        if(pid == 0){
+            execvp(args[0], args);
+            perror("erro");
+            exit(1);
+        } 
+        else if(pid > 0){
+            wait(NULL);
+        } 
+        else{
+            perror("fork falhou");
         }
     }
 
