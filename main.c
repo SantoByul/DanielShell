6d1ec4c00915955a603493930fc15e4a8c33302d
Daniel S. <danielbarbosa27@icloud.com>
2026-03-06 11:04:55 -0300

    outro reset

--- main.c
+++ main.c
@@ -7,42 +7,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-void lsh_loop(void);
-char* lsh_read_line(void);
-char** lsh_split_line(char *line);
-int status_lsh_execute(char **args);
+int main(){
 
-void lsh_loop(void){
-    char *line;
-    char **args;
-    int status;
-
-    do{
-        printf("DanielShell> ");
-
-        line=lsh_read_line();
-        args=lsh_split_line(line);
-        status=status_lsh_execute(args);
-
-        free(line);
-        free(args);
-    }while(status);
-}
-
-char* lsh_read_line(void){
-    return NULL;
-}
-
-char** lsh_split_line(char *line){
-    return NULL;
-}
-
-int status_lsh_execute(char **args){
+`
     return 0;
-}
-
-int main(int argc, char **argv){
-
-    lsh_loop();
-    return EXIT_SUCCESS;
 }
